8627c70c12070e928fe85768a6324365559f32ca
IceDynamix <22303902+IceDynamix@users.noreply.github.com>
2023-01-08 23:47:17 +0100

    Add modal footer slot

--- index.js
+++ index.js
@@ -167,10 +167,11 @@ app.component('modal', {
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
-            <slot></slot>
+            <slot name="body"></slot>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <slot name="footer"></slot>
           </div>
         </div>
       </div>
